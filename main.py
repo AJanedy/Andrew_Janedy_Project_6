b5d5d312fda10020ec087b54478dde7eaf874279
andrewjanedy <andy.janedy@gmail.com>
2022-11-30 14:39:38 -0500

    moving zombies and elimination

--- main.py
+++ main.py
@@ -19,20 +19,44 @@ class GameWindow(arcade.Window):
         self.player.center_x = 600
         self.player.center_y = 65
 
+        for number in range(3):
+            enemy = arcade.Sprite(":resources:images/animated_characters/zombie/zombie_walk5.png")
+            enemy.center_x = random.randint(36, 1164)
+            enemy.center_y = 664
+            self.enemy1.append(enemy)
+
     def on_draw(self):
         arcade.start_render()
 
         self.player.draw()
         self.fire.draw()
+        self.enemy1.draw()
 
         arcade.finish_render()
 
     def on_update(self, delta_time):
+
         self.player.center_x += self.player_dx
         if self.player.center_x > 1200:
             self.player.center_x = 0
         if self.player.center_x < 0:
             self.player.center_x = 1200
+
+        for sprite in self.fire:
+            if sprite.center_y < 800:
+                sprite.center_y += 10
+
+        for sprite in self.enemy1:
+            if sprite.center_y > - 74:
+                sprite.center_y -= 1
+
+        for sprite in self.fire:
+            shot_zombies = arcade.check_for_collision_with_list(sprite, self.enemy1)
+            if shot_zombies:
+                #arcade.play_sound(self.sound)
+                self.score += len(shot_zombies)
+                for extra_dead_zombies in shot_zombies:
+                    self.enemy1.remove(extra_dead_zombies)
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.player.center_x = x
